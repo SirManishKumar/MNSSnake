f805de33862e4ed063474ac6cfb4cc8f313fecb6
Manish Kumar <79128476+SirManishKumar@users.noreply.github.com>
2021-05-19 07:49:10 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,78 +1,115 @@
 # SNAKES GAME BY SirManishKumar
-# Use ARROW KEYS to play, SPACE BAR for pausing/resuming and Esc Key for exiting
-
+# -*- coding: utf-8 -*- 
 import curses
-from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint
 
+class SnakeGame:
+    def __init__(self, board_width = 20, board_height = 20, gui = False):
+        self.score = 0
+        self.done = False
+        self.board = {'width': board_width, 'height': board_height}
+        self.gui = gui
 
-curses.initscr()
-win = curses.newwin(20, 60, 0, 0)
-win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
-win.border(0)
-win.nodelay(1)
+    def start(self):
+        self.snake_init()
+        self.generate_food()
+        if self.gui: self.render_init()
+        return self.generate_observations()
 
-key = KEY_RIGHT                                                    # Initializing values
-score = 0
+    def snake_init(self):
+        x = randint(5, self.board["width"] - 5)
+        y = randint(5, self.board["height"] - 5)
+        self.snake = []
+        vertical = randint(0,1) == 0
+        for i in range(3):
+            point = [x + i, y] if vertical else [x, y + i]
+            self.snake.insert(0, point)
 
-snake = [[4,10], [4,9], [4,8]]                                     # Initial snake co-ordinates
-food = [10,20]                                                     # First food co-ordinates
-
-win.addch(food[0], food[1], '*')                                   # Prints the food
-
-while key != 27:                                                   # While Esc key is not pressed
-    win.border(0)
-    win.addstr(0, 2, 'Score : ' + str(score) + ' ')                # Printing 'Score' and
-    win.addstr(0, 27, ' SNAKE ')                                   # 'SNAKE' strings
-    win.timeout(150 - (len(snake)/5 + len(snake)/10)%120)          # Increases the speed of Snake as its length increases
-    
-    prevKey = key                                                  # Previous key pressed
-    event = win.getch()
-    key = key if event == -1 else event 
-
-
-    if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
-        key = -1                                                   # one (Pause/Resume)
-        while key != ord(' '):
-            key = win.getch()
-        key = prevKey
-        continue
-
-    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
-        key = prevKey
-
-    # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
-    # This is taken care of later at [1].
-    snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
-
-    # If snake crosses the boundaries, make it enter from the other side
-    if snake[0][0] == 0: snake[0][0] = 18
-    if snake[0][1] == 0: snake[0][1] = 58
-    if snake[0][0] == 19: snake[0][0] = 1
-    if snake[0][1] == 59: snake[0][1] = 1
-
-    # Exit if snake crosses the boundaries (Uncomment to enable)
-    #if snake[0][0] == 0 or snake[0][0] == 19 or snake[0][1] == 0 or snake[0][1] == 59: break
-
-    # If snake runs over itself
-    if snake[0] in snake[1:]: break
-
-    
-    if snake[0] == food:                                            # When snake eats the food
+    def generate_food(self):
         food = []
-        score += 1
         while food == []:
-            food = [randint(1, 18), randint(1, 58)]                 # Calculating next food's coordinates
-            if food in snake: food = []
-        win.addch(food[0], food[1], '*')
-    else:    
-        last = snake.pop()                                          # [1] If it does not eat the food, length decreases
-        win.addch(last[0], last[1], ' ')
-    win.addch(snake[0][0], snake[0][1], '#')
-    
-curses.endwin()
-print("\nScore - " + str(score))
+            food = [randint(1, self.board["width"]), randint(1, self.board["height"])]
+            if food in self.snake: food = []
+        self.food = food
+
+    def render_init(self):
+        curses.initscr()
+        win = curses.newwin(self.board["width"] + 2, self.board["height"] + 2, 0, 0)
+        curses.curs_set(0)
+        win.nodelay(1)
+        win.timeout(200)
+        self.win = win
+        self.render()
+
+    def render(self):
+        self.win.clear()
+        self.win.border(0)
+        self.win.addstr(0, 2, 'Score : ' + str(self.score) + ' ')
+        self.win.addch(self.food[0], self.food[1], '🍎')
+        for i, point in enumerate(self.snake):
+            if i == 0:
+                self.win.addch(point[0], point[1], '🔸')
+            else:
+                self.win.addch(point[0], point[1], '🔹')
+        self.win.getch()
+
+    def step(self, key):
+        # 0 - UP
+        # 1 - RIGHT
+        # 2 - DOWN
+        # 3 - LEFT
+        if self.done == True: self.end_game()
+        self.create_new_point(key)
+        if self.food_eaten():
+            self.score += 1
+            self.generate_food()
+        else:
+            self.remove_last_point()
+        self.check_collisions()
+        if self.gui: self.render()
+        return self.generate_observations()
+
+    def create_new_point(self, key):
+        new_point = [self.snake[0][0], self.snake[0][1]]
+        if key == 0:
+            new_point[0] -= 1
+        elif key == 1:
+            new_point[1] += 1
+        elif key == 2:
+            new_point[0] += 1
+        elif key == 3:
+            new_point[1] -= 1
+        self.snake.insert(0, new_point)
+
+    def remove_last_point(self):
+        self.snake.pop()
+
+    def food_eaten(self):
+        return self.snake[0] == self.food
+
+    def check_collisions(self):
+        if (self.snake[0][0] == 0 or
+            self.snake[0][0] == self.board["width"] + 1 or
+            self.snake[0][1] == 0 or
+            self.snake[0][1] == self.board["height"] + 1 or
+            self.snake[0] in self.snake[1:-1]):
+            self.done = True
+
+    def generate_observations(self):
+        return self.done, self.score, self.snake, self.food
+
+    def render_destroy(self):
+        curses.endwin()
+
+    def end_game(self):
+        if self.gui: self.render_destroy()
+        raise Exception("Game over")
+
+if __name__ == "__main__":
+    game = SnakeGame(gui = True)
+    game.start()
+    for _ in range(20):
+        game.step(randint(0,3))
+
 print("GAME OVER\n")
 print("BY SirManishKumar\n")
